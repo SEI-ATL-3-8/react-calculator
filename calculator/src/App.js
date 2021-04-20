b9a1cbd4075d447ff462c7898fb4cd46158c2f1f
Mason <masonbush751@gmail.com>
2021-04-20 17:24:21 -0500

    Mason HW

---
+++ calculator/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [secondNumber, setSecondNumber] = useState(0)
+  const [operator, setOperator] = useState('+')
+
+  const calculateNumber = e => {
+    e.preventDefault()
+    switch(operator) {
+      case '*':
+        document.querySelector('h1').innerText = firstNumber * secondNumber
+        break;
+      case '+':
+        document.querySelector('h1').innerText = parseInt(firstNumber) + parseInt(secondNumber)
+        break;
+      case '-':
+        document.querySelector('h1').innerText = firstNumber - secondNumber
+        break;
+      case '%':
+        document.querySelector('h1').innerText = firstNumber / secondNumber
+        break;
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <form onSubmit={calculateNumber}>
+          <input type='number' name='firstNumber' value={firstNumber} placeholder='First Digit' onChange={e => setFirstNumber(e.target.value)} />
+          <button type='button' value='*' onClick={() => { setOperator('*') }}>*</button>
+          <button type='button' value='+' onClick={() => { setOperator('+') }}>+</button>
+          <button type='button' value='-' onClick={() => { setOperator('-') }}>-</button>
+          <button type='button' value='%' onClick={() => { setOperator('%') }}>%</button>
+          <input type='number' name='secondNumber' value={secondNumber} placeholder='Second Digit' onChange={e => setSecondNumber(e.target.value)} />
+          <input type='submit' value='Calculate' />
+        </form>
+        <h1></h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;

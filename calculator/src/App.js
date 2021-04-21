869b8539ab6f3c50a53ae4ce236f1478d3961b40
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-04-20 23:29:20 -0400

    core goals

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,52 @@
 
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+ 
+  const [numberOne, setNumberOne] = useState('')
+  const [numberTwo, setNumberTwo] = useState('')
+  const [result, setResult] = useState('')
+
+   const addition = () => {
+     setResult(numberOne + numberTwo);
+   }
+   const subtract = () => {
+     setResult(numberOne - numberTwo)
+   }
+   const multiplication = () => {
+     setResult(numberOne * numberTwo)
+   }
+   const division = () => {
+     setResult(numberOne / numberTwo)
+   }
+
+  
+
   return (
     <div className="App">
-     <h1>Calculator!</h1>
+     
+     <form>
+     <input type="number" name="numberOne"
+     value ={numberOne} onChange={(e) => setNumberOne(+e.target.value)}/>
+    </form>
+    <div className="grid">
+      <button onClick={addition}>+</button>
+      <button onClick={subtract}>-</button>
+      <button onClick={multiplication}>x</button> 
+      <button onClick={division}>/</button>
+    </div>
+    <form>
+     <input type="number" name="numberTwo"
+     value ={numberTwo} onChange={(e) => setNumberTwo(+e.target.value)}/>
+    </form>
+    <div className="grid">
+      <span>=</span>
+    </div>
+    <div className="result">
+    <span>{result}</span>
+    </div>
+     
     </div>
   );
 }

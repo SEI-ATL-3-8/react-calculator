42ebead2e667b80f09fe8e3a13ab530f6da5a8e3
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-04-20 20:20:49 -0600

    clement-mwimo-hw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
 function App() {
+  const [number1, setNumber1] = useState(0)
+  const [number2, setNumber2] = useState(0)
+  const [result, setResult] = useState(0)
+  const [operation, setOperation] = useState('addition')
+
+  const handleOperation = () => {
+    switch (operation) {
+      case 'substraction':
+        setResult(number1 - number2)
+        break;
+      case 'multiplication':
+        setResult(number1 * number2)
+        break;
+      case 'division':
+        setResult(number1 / number2)
+        break;
+      default:
+        setResult(number1 + number2)
+        break;
+
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" name="number1" onChange={(e) => setNumber1(parseInt(e.target.value))} />
+      <button onClick={() => setOperation('addition')}>+</button>
+      <button onClick={() => setOperation('substraction')}>-</button>
+      <button onClick={() => setOperation('multiplication')}>*</button>
+      <button onClick={() => setOperation('division')}>/</button>
+      <input type="text" name="number2" onChange={(e) => setNumber2(parseInt(e.target.value))} />
+      <button onClick={handleOperation}>=</button>
+      <span>{result ? result : null}</span>
     </div>
   );
 }

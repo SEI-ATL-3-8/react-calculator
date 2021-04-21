0403a92028fde1b21a44715ad196626259d1abf0
Tim Mok <timothyjmok@gmail.com>
2021-04-21 00:25:29 -0400

    created another reusable function to clean up code in stretch.js

--- calculator/src/Stretch.js
+++ calculator/src/Stretch.js
@@ -7,104 +7,93 @@ let calculation = []
 const Stretch = () => {
 
     const [number, setNumber] = useState(0)
+
+    // state to keep track of second most recent operator
     const [operator, setOperator] = useState(null)
+
+    // state to keep track of most recent operator
+    const [math, setMath] = useState(null)
 
     // assigns current number state
     const onChangeNumField = (e) => {
         setNumber(parseFloat(e.target.value))
     }
 
-    // callback to calculate current pair of numbers based on most recent operator state
-    const flexCalc = (math) => {
-        if ( calculation.length === 2 && math === 'add' ) {
+    // callback to calculate current pair of numbers based on most recent operator state before the most recently pressed operator's calculation
+    const flexCalc = (operatorState) => {
+        if ( calculation.length === 2 && operatorState === 'add' ) {
             setNumber(calculation[0] + calculation[1])
             calculation.push(calculation[0] + calculation[1])
             calculation.shift()
             calculation.shift()
-        } else if ( calculation.length === 2 && math === 'subtract' ) {
+        } else if ( calculation.length === 2 && operatorState === 'subtract' ) {
             setNumber(calculation[0] - calculation[1])
             calculation.push(calculation[0] - calculation[1])
             calculation.shift()
             calculation.shift()           
-        } else if ( calculation.length === 2 && math === 'multiply' ) {
+        } else if ( calculation.length === 2 && operatorState === 'multiply' ) {
             setNumber(calculation[0] * calculation[1])
             calculation.push(calculation[0] * calculation[1])
             calculation.shift()
             calculation.shift()
-        } else if ( calculation.length === 2 && math === 'divide' ) {
+        } else if ( calculation.length === 2 && operatorState === 'divide' ) {
             setNumber(calculation[0] / calculation[1])
             calculation.push(calculation[0] / calculation[1])
             calculation.shift()
             calculation.shift()
+        }
+    }
+
+    // callback to calculate current pair of numbers based on most recently pressed operator's calculation
+    const buttonCalc = (mathType, operatorType) => {
+        if ( calculation.length === 2 ) {
+            setNumber(parseFloat(`${calculation[0]} ${mathType} ${calculation[1]}`))
+            calculation.push(parseFloat(`${calculation[0]} ${mathType} ${calculation[1]}`))
+            calculation.shift()
+            calculation.shift()
+        } else {
+            setOperator(operatorType)
         }
     }
 
     // connected to add button, runs callback first before proceeding
     const addCalc = () => {
+        setMath('+')
         calculation.push(number)
         if ( operator !== null ) {
             flexCalc(operator)
         }
-        if ( calculation.length === 2 ) {
-            setNumber(calculation[0] + calculation[1])
-            calculation.push(calculation[0] + calculation[1])
-            calculation.shift()
-            calculation.shift()
-        } else {
-            console.log('calculation', calculation)
-            setOperator('add')            
-        }
+        buttonCalc(math, 'add')
     }
 
     // connected to subtract button, runs callback first before proceeding
     const subtractCalc = () => {
+        setMath('-')
         calculation.push(number)
         if ( operator !== null ) {
             flexCalc(operator)
         }
-        if ( calculation.length === 2 ) {
-            setNumber(calculation[0] - calculation[1])
-            calculation.push(calculation[0] - calculation[1])
-            calculation.shift()
-            calculation.shift()
-        } else {
-            console.log('calculation', calculation)
-            setOperator('subtract')            
-        }
+        buttonCalc(math, 'subtract')
     }
 
     // connected to multiply button, runs callback first before proceeding
     const multiplyCalc = () => {
+        setMath('*')
         calculation.push(number)
         if ( operator !== null ) {
             flexCalc(operator)
         }
-        if ( calculation.length === 2 ) {
-            setNumber(calculation[0] * calculation[1])
-            calculation.push(calculation[0] * calculation[1])
-            calculation.shift()
-            calculation.shift()
-        } else {
-            console.log('calculation', calculation)
-            setOperator('multiply')
-        }
+        buttonCalc(math, 'multiply')
     }
 
     // connected to divide button, runs callback first before proceeding
     const divideCalc = () => {
+        setMath('/')
         calculation.push(number)
         if ( operator !== null ) {
             flexCalc(operator)
         }
-        if ( calculation.length === 2 ) {
-            setNumber(calculation[0] / calculation[1])
-            calculation.push(calculation[0] / calculation[1])
-            calculation.shift()
-            calculation.shift()
-        } else {
-            console.log('calculation', calculation)
-            setOperator('divide')
-        }
+        buttonCalc(math, 'divide')
     }
 
     // if the user wants to get the final result and stop the calculation "run"
@@ -118,6 +107,8 @@ const Stretch = () => {
     // if the user wants to clear the calculator's "memory" and start with fresh calculations
     const clearCalc = () => {
         setNumber(0)
+        setOperator(null)
+        setMath(null)
         calculation.length = 0
     }
 

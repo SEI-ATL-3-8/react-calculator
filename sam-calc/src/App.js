2a138fc411fa99af06ab7ec9f6bb44cebc9aea79
sblum55 <healysa55@gmail.com>
2021-04-20 21:10:27 -0400

    sam-branch

--- sam-calc/src/App.js
+++ sam-calc/src/App.js
@@ -5,18 +5,36 @@ function App() {
   const [firstNumber, setFirstNumber] = useState(0)
   const [secondNumber, setSecondNumber] = useState(0)
   const [totalNumber, setTotalNumber] = useState(0)
+  const [buttonValue, setButtonValue] = useState('')
 
+const submitTotal = () => {
+  if (buttonValue === '+' ) {
+    setTotalNumber(parseInt(firstNumber) + parseInt(secondNumber))
+  } else if (buttonValue === '-') {
+    setTotalNumber(parseInt(firstNumber) - parseInt(secondNumber))
+  } else if (buttonValue === '*') {
+    setTotalNumber(parseInt(firstNumber) * parseInt(secondNumber))
+  } else if (buttonValue === '%') {
+    setTotalNumber(parseInt(firstNumber) / parseInt(secondNumber))
+  }
+    // console.log(totalNumber);
+    // console.log(totalNumber);
+}
 
   return (
     <div className="App">
       <h1>Calculator</h1>
-     <input type = 'text' name = 'firstNumber' value = {firstNumber} onChange = {(e) => setFirstNumber(e.target.value)}/>
-     <input type = 'text' name = 'secondNumber' value = {secondNumber} onChange = {(e) => setSecondNumber(e.target.value)}/>
-     <input type = 'submit' value = '+' />
-     <input type = 'submit' value = '-' />
-     <input type = 'submit' value = '*' />
-     <input type = 'submit' value = '%' />
-     <h2>{totalNumber}</h2>
+     <input type = 'select' name = 'firstNumber' value = {firstNumber} onChange = {(e) => setFirstNumber(e.target.value)}/>
+
+     <input type = 'button' value = '+' onClick = {(e) => setButtonValue(e.target.value)} />
+     <input type = 'button' value = '-' onClick = {(e) => setButtonValue(e.target.value)} />
+     <input type = 'button' value = '*' onClick = {(e) => setButtonValue(e.target.value)} />
+     <input type = 'button' value = '%' onClick = {(e) => setButtonValue(e.target.value)} />
+
+     <input type = 'select' name = 'secondNumber' value = {secondNumber} onChange = {(e) => setSecondNumber(e.target.value)}/>
+     <h2>
+       <button onClick = {submitTotal}>=</button>
+        {totalNumber.toFixed(2)}</h2>
     </div>
   );
 }

7d954f0a04513c07603b0e7bd39f56bd3db7e420
Charlesiubao <kristoffer.charles.reyes@gmail.com>
2021-04-20 21:16:33 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [first, setFirst] = useState();
+  const [second, setSecond] = useState();
+  const [result, setResult] = useState();
+
+  const add = () => {
+    setResult(first + second);
+  }
+
+  const minus = () => {
+    setResult(first - second);
+  }
+
+  const times = () => {
+    setResult(first * second);
+  }
+
+  const over = () => {
+    setResult(first / second);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+
+      <div className = 'inputs'>
+
+      <input type = 'text' className = 'input' placeholder = 'first number' value = {first} onChange = {(e) => setFirst(Number(e.target.value))} />
+
+      <input type = 'text' className = 'input' placeholder = 'second number' value = {second} onChange = {(e) => setSecond(Number(e.target.value))} />
+
+      </div>
+      <div className = 'buttons'>
+      <button className = 'button' onClick = {add}>+</button>
+      <button className = 'button' onClick = {minus}>-</button>
+      <button className = 'button' onClick = {times}>x</button>
+      <button className = 'button' onClick = {over}>÷</button>
+      </div>
+      <h3 className = 'result'>= {result}</h3>
     </div>
   );
 }

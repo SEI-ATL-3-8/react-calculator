4fb14545c6006345de96d32d5fc3ea038f210ef6
Derek Ensign <derekensign@gmail.com>
2021-04-20 23:44:32 -0500

    completed calc functionality

--- react-calc/src/App.js
+++ react-calc/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [secondNumber, setSecondNumber] = useState(0)
+  const [resultNumber, setResultNumber] = useState(0)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('form submit')
+    console.log(firstNumber, secondNumber, resultNumber)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit = {handleSubmit}>
+        <input type="number" name="firstNumber" value={firstNumber}
+         onChange={(e) => setFirstNumber(e.target.value)}/>
+
+         <button onClick ={() => { setResultNumber(Number(firstNumber) + Number(secondNumber))}}>+</button>
+         <button onClick ={() => { setResultNumber(Number(firstNumber) - Number(secondNumber))}}>-</button>
+         <button onClick ={() => { setResultNumber(Number(firstNumber) * Number(secondNumber))}}>x</button>
+         <button onClick ={() => { setResultNumber(Number(firstNumber) / Number(secondNumber))}}>/</button>
+
+        <input type="number" name="secondNumber" value = {secondNumber}
+        onChange={(e) => setSecondNumber(e.target.value)}/>
+        <label for="resultNumber">Answer:</label>
+        <input type="number" name="resultNumber" value = {resultNumber}/>       
+      </form>
+      
     </div>
   );
 }
